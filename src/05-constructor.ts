24171fea997eaabd21a9fb283811ae105d8726c0
Pau Egea <papau84@gmail.com>
2022-04-03 23:11:55 +0200

    constructor

--- src/05-constructor.ts
+++ src/05-constructor.ts
@@ -1,20 +1,17 @@
 export class MyDate {
-  public year: number;
-  public month: number;
-  private day: number;
 
-  constructor(year: number, month: number, day: number) {
-    this.year = year;
-    this.month = month;
-    this.day = day;
-  }
+  constructor(
+    public year: number = 1993,
+    public month: number = 7,
+    private day: number = 9
+  ) { }
 
   public printFormat(): string {
     const day = this.addPadding(this.day);
     const month = this.addPadding(this.month);
     return `${day}/${month}/${this.year}`;
   }
-  private addPadding(value: number){
+  private addPadding(value: number) {
     if (value < 10) {
       return `0${value}`;
     } return value;
@@ -42,8 +39,11 @@ console.log(myDate.printFormat());
 //console.log(myDate.day);
 console.log(myDate.month);
 console.log(myDate.year);
-//myDate.day = 10;
-//console.log(myDate.day);
-console.log(myDate.getDay());
+
+const myDate2 = new MyDate();
+console.log('MyDate2 ' + myDate2.printFormat());
+console.log('MyDate2 ' + myDate2.getDay());
 
 
+
+//
