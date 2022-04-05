8098ac470165cc6edb23d359e1f00ccc85e1d49e
Pau Egea <papau84@gmail.com>
2022-04-05 21:58:01 +0200

    generics metodos

--- src/app/service/base-http.service.ts
+++ src/app/service/base-http.service.ts
@@ -1,34 +1,46 @@
-import { Category } from "../models/category.model";
-import axios from "axios";
-import { Product } from "../models/product.model";
+import axios from 'axios';
+import { UpdateProductDto } from '../dto/product.dto';
 
-export class BaseHttpService<typeClass> {
-  // data: typeClass[] = [];
-  //private url = 'https://api.escuelajs.co/api/v1/products';
+import { Category } from './../models/category.model';
+import { Product } from './../models/product.model';
+
+export class BaseHttpService<TypeClass> {
+  // data: TypeClass[] = [];
+
   constructor(
-    private url : string
-  ){}
+    protected url: string
+  ) {}
 
-  async getAll(){
-    const {data} = await axios.get<typeClass[]>(this.url);
+  async getAll() {
+    const { data } = await axios.get<TypeClass[]>(this.url);
+    return data;
+  }
+
+  async update<ID, DTO>(id: ID, changes: DTO) {
+    const { data } = await axios.put(`${this.url}/${id}`, changes);
     return data;
   }
 }
 
-
-(async () => {
-  const productsService = new BaseHttpService<Product>('https://api.escuelajs.co/api/v1/products');
-  const rta = await productsService.getAll();
-  console.log(rta.length);
-
-  const productsService2 = new BaseHttpService<Product>('https://api.escuelajs.co/api/v1/categories');
-  const rta2 = await productsService2.getAll();
-  console.log(rta2.length);
-
-})();
 // const service = new BaseHttpService<string>();
-// service.getAll();
+// service.getAll()
 
-// const service1 = new BaseHttpService<number>();
-// service1.getAll();
+// const service1 = new BaseHttpService<Category>();
+// service1.getAll
 
+(async ()=> {
+  const url1 = 'https://api.escuelajs.co/api/v1/products';
+  const productService = new BaseHttpService<Product>(url1);
+
+  const rta = await productService.getAll();
+  console.log('products', rta.length);
+  productService.update<Product['id'], UpdateProductDto>(1, {
+    title: 'asa',
+  });
+
+  const url2 = 'https://api.escuelajs.co/api/v1/categories';
+  const categoryService = new BaseHttpService<Category>(url2);
+
+  const rta1 = await categoryService.getAll();
+  console.log('categories', rta1.length);
+})();
